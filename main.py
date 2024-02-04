ab99adfd128f73018244ebd862b5f4b100c77113
Matthew Thomas <adriant172@gmail.com>
2024-02-05 08:47:56 +0900

    add line creation functionality

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 """Custom module for rendering the game window"""
 
-from window import Window
+from window import Window, Point, Line
 
 
 def main():
     """main function for running all functions"""
     win = Window(800, 600)
+    point_a = Point(450,370)
+    point_b = Point(180, 600)
+    new_line = Line(point_a,point_b)
+    win.draw_line(new_line,"blue")
     win.wait_for_close()
-
 
 main()
--- window.py
+++ window.py
@@ -8,8 +8,8 @@ class Window:
         self.height = height
         self.__root = Tk()
         self.__root.title("It all starts here!")
-        self.canvas = Canvas(self.__root, bg="black", height=self.height, width=self.width)
-        self.canvas.pack()
+        self.canvas = Canvas(self.__root, bg="white", height=self.height, width=self.width)
+        self.canvas.pack(fill=BOTH, expand=1)
         self.window_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -28,20 +28,25 @@ class Window:
     def close(self):
         """This toggles the window running boolean to off"""
         self.window_running = False
+    
+    def draw_line(self,line,color):
+        line.draw(self.canvas, color)
 
 class Point:
-
+    """Simple class to create points based ona X and Y axis"""
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 class Line:
-
+    """ This class creates a line between two points"""   
     def __init__(self, a,b):
         self.a = a
         self.b = b
-    
-    def draw(canvas, color):
-        canvas.create_line(self.a.x, self.a.y,self.)
+    def draw(self, canvas, color):
+        canvas.create_line(
+            self.a.x, self.a.y,self.b.x, self.b.y, fill=color, width=2
+            )
+        canvas.pack(fill=BOTH, expand=1)
 
          
