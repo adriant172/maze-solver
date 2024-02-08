3e00972bf6e3e04f0afe7ac7afb35e4a00bc2c14
Matthew Thomas <adriant172@gmail.com>
2024-02-08 21:51:29 +0900

    fix the draw_move function for the Cell class

--- cell.py
+++ cell.py
@@ -3,16 +3,17 @@ from window import Point, Line
 
 class Cell:
     """This class will create a box that lives on the windows grid. It takes the X and Y coordinates of the top-left point"""
-    def __init__(self,x,y, window):
+    def __init__(self,x1,y1,x2,y2, window):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self.__x1 = x
-        self.__x2 = y
-        self.__y1 = y
-        self.__y2 = x
-        self.__center_num = (self.__y1 + self.__x1) / 2
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+        self.center_x = self.__x1 + (self.__x2 - self.__x1) / 2
+        self.center_y = self.__y1 + (self.__y2 - self.__y1) / 2
         self.__win = window
 
 
@@ -22,9 +23,6 @@ class Cell:
     def get_y1(self):
         """Getter method for the y1 coordinate"""
         return self.__y1
-    def get_center_num(self):
-        """Getter method for the center point number"""
-        return self.__center_num
     def draw(self, color):
         """Draw the walls of the cell"""
         top_left_point = Point(self.__x1, self.__y1)
@@ -48,16 +46,12 @@ class Cell:
             line_color = "red"
         else:
             line_color = "grey"
-        center_point = Point(self.__center_num, self.__center_num)
-        to_cell_center = Point(to_cell.get_center_num(), to_cell.get_center_num())
-        if to_cell.get_x1() > self.__y1 or to_cell.get_x1() < self.__y1:
-            if to_cell.get_x1() > self.__x2 or to_cell.get_x1() < self.__x2 :
-                turn_point = Point(to_cell.get_center_num(),self.__center_num)
-                self.__win.draw_line(Line(center_point, turn_point), line_color)
-            to_cell_center = Point(to_cell.get_center_num(), to_cell.get_center_num())
+        center_point = Point(self.center_x, self.center_y)
+        to_cell_center = Point(to_cell.center_x, to_cell.center_y)
+        if to_cell.get_y1() != self.__y1:
+            turn_point = Point(to_cell.center_x, self.center_y)
+            self.__win.draw_line(Line(center_point, turn_point), line_color)
             self.__win.draw_line(Line(turn_point, to_cell_center), line_color)
 
-            
-            
-            
-        
+        if to_cell.get_y1() == self.__y1:  # Cells are in the same row
+            self.__win.draw_line(Line(center_point,to_cell_center), line_color)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from cell import Cell
 
 def main():
     """main function for running all functions"""
-    win = Window(1000, 800)
+    win = Window(1000, 1000)
     # for i in range(50, 600, 50):
     #     new_cell = Cell(i, i + 50, win)
     #     if i % 100 == 0:
@@ -13,10 +13,15 @@ def main():
     #         new_cell.has_right_wall = False
     #     new_cell.draw("black")
     # win.draw_line(new_line,"blue")
-    cell_1 = Cell(250, 300, win)
-    cell_2 = Cell(350, 400, win)
+    cell_1 = Cell(750,750,800,800,win)
+    cell_1.has_right_wall = False
+    cell_1.has_left_wall = False
+    cell_2 = Cell(650,650,700,700, win)
+    cell_2.has_bottom_wall = False
+    # cell_3 = Cell(800,700,900,800, win)
     cell_1.draw("black")
     cell_2.draw("blue")
+   
     cell_1.draw_move(cell_2, undo=True)
     win.wait_for_close()
 
